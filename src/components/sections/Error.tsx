e6ac4a5e6bd3661cf60ce205c0170ecac33d82b9
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2023-10-12 10:31:26 +0200

    Init project

---
+++ src/components/sections/Error.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import {useTranslations} from "use-intl";
+import styles from '@styles/error.module.css';
+
+interface Props {
+    title: string;
+    description: string;
+}
+
+export default function Error(props : Props): JSX.Element {
+
+    const t = useTranslations("error");
+
+    return (
+        <div className={styles.error_page}>
+            <div className={styles.error_container}>
+                <h1 className={styles.error_title}>
+                    {props.title}
+                </h1>
+                <p className={styles.error_description}>
+                    {props.description}
+                </p>
+                <div className={styles.error_button_container}>
+                    <Link href="/" className="" scroll={false} onClick={() => window.scrollTo(0, 0)} passHref>
+                        <button
+                            className={styles.error_button}
+                        >
+                            {t("back")}
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
